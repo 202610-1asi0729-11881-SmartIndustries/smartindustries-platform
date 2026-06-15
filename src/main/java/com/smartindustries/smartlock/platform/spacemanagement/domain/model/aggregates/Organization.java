32c327494deefe30b7221fdd64e9e1620e9e12ff
ImaLi06 <imanollimache@gmail.com>
2026-06-15 01:05:18 -0500

    feat(spacemanagement): create Organization aggregate

---
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/domain/model/aggregates/Organization.java
@@ -0,0 +1,31 @@
+package com.smartindustries.smartlock.platform.spacemanagement.domain.model.aggregates;
+
+import com.smartindustries.smartlock.platform.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
+import com.smartindustries.smartlock.platform.shared.domain.model.valueobjects.GenericName;
+import com.smartindustries.smartlock.platform.spacemanagement.domain.model.commands.CreateOrganizationCommand;
+import com.smartindustries.smartlock.platform.spacemanagement.domain.model.events.OrganizationCreatedEvent;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Organization extends AbstractDomainAggregateRoot<Organization> {
+
+    @Setter
+    private Long id;
+
+    private GenericName name;
+
+    private String description;
+
+    public Organization() {
+    }
+
+    public Organization(CreateOrganizationCommand command) {
+        this.name = new GenericName(command.name());
+        this.description = command.description();
+    }
+
+    public void onCreate() {
+        this.registerDomainEvent(new OrganizationCreatedEvent(this, this.getId(), this.name.value()));
+    }
+}
