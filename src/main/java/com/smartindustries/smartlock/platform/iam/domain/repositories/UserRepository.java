d921b07128fa8d337eda2cae549dfc53a5b04a98
ImaLi06 <imanollimache@gmail.com>
2026-06-14 21:20:31 -0500

    feat(iam): create UserRepository

---
+++ src/main/java/com/smartindustries/smartlock/platform/iam/domain/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.smartindustries.smartlock.platform.iam.domain.repositories;
+
+import com.smartindustries.smartlock.platform.iam.domain.model.aggregates.User;
+
+public interface UserRepository {
+    User save(User user);
+}
