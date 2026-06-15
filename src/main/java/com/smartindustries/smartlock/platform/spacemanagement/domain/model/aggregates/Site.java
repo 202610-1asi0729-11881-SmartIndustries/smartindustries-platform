a5204eaa65db6270773d42730530d91f8b0b565c
ImaLi06 <imanollimache@gmail.com>
2026-06-15 04:41:45 -0500

    feat(spacemanagement): create Site aggregate

---
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/domain/model/aggregates/Site.java
@@ -0,0 +1,32 @@
+package com.smartindustries.smartlock.platform.spacemanagement.domain.model.aggregates;
+
+import com.smartindustries.smartlock.platform.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
+import com.smartindustries.smartlock.platform.shared.domain.model.valueobjects.GenericName;
+import com.smartindustries.smartlock.platform.spacemanagement.domain.model.commands.AddSiteToOrganizationCommand;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Site extends AbstractDomainAggregateRoot<Site> {
+
+    @Setter
+    private Long id;
+
+    @Setter
+    private Long organizationId;
+
+    @Setter
+    private GenericName name;
+
+    @Setter
+    private String description;
+
+    public Site() {
+    }
+
+    public Site(AddSiteToOrganizationCommand command) {
+        this.organizationId = command.organizationId();
+        this.name = new GenericName(command.name());
+        this.description = command.description();
+    }
+}
