3be755dd59431965c03832e54abae0df1cd2fc4e
ImaLi06 <imanollimache@gmail.com>
2026-06-15 04:45:39 -0500

    feat(spacemanagement): create SitePersitenceEntity

--- src/main/java/com/smartindustries/smartlock/platform/spacemanagement/infrastructure/persistence/jpa/entities/SitePersistenceEntity.java
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/infrastructure/persistence/jpa/entities/SitePersistenceEntity.java
@@ -1,6 +1,5 @@
 package com.smartindustries.smartlock.platform.spacemanagement.infrastructure.persistence.jpa.entities;
 
-import com.smartindustries.smartlock.platform.administration.infrastructure.persistence.jpa.entities.RolePersistenceEntity;
 import com.smartindustries.smartlock.platform.shared.domain.model.valueobjects.GenericName;
 import com.smartindustries.smartlock.platform.shared.infrastructure.persistence.jpa.converters.GenericNamePersistenceConverter;
 import com.smartindustries.smartlock.platform.shared.infrastructure.persistence.jpa.entities.AuditableAbstractPersistenceEntity;
@@ -9,15 +8,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
-@Table(name = "organizations")
+@Table(name = "sites")
 @Getter
 @Setter
 @NoArgsConstructor
-public class OrganizationPersistenceEntity extends AuditableAbstractPersistenceEntity {
+public class SitePersistenceEntity extends AuditableAbstractPersistenceEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id", nullable = false)
+    private OrganizationPersistenceEntity organization;
 
     @Convert(converter = GenericNamePersistenceConverter.class)
     @Column(name = "name", nullable = false, length = 100)
@@ -25,10 +25,4 @@ public class OrganizationPersistenceEntity extends AuditableAbstractPersistenceE
 
     @Column(name = "description", nullable = false, length = 500)
     private String description;
-
-    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<RolePersistenceEntity> roles = new ArrayList<>();
-
-    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<SitePersistenceEntity> sites = new ArrayList<>();
 }
