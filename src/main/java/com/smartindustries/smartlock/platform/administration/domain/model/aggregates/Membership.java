3be7ba9296520e412a86f10bc2d8d7b24c998213
ImaLi06 <imanollimache@gmail.com>
2026-06-15 03:47:11 -0500

    feat(administration): create Membership aggregate

---
+++ src/main/java/com/smartindustries/smartlock/platform/administration/domain/model/aggregates/Membership.java
@@ -0,0 +1,27 @@
+package com.smartindustries.smartlock.platform.administration.domain.model.aggregates;
+
+import com.smartindustries.smartlock.platform.administration.domain.model.commands.AddRootUserToOrganizationCommand;
+import com.smartindustries.smartlock.platform.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Membership extends AbstractDomainAggregateRoot<Membership> {
+
+    @Setter
+    private Long id;
+
+    @Setter
+    private Long userId;
+
+    @Setter
+    private Long roleId;
+
+    public Membership() {
+    }
+
+    public Membership(AddRootUserToOrganizationCommand command) {
+        this.userId = command.userId();
+        this.roleId = command.roleId();
+    }
+}
