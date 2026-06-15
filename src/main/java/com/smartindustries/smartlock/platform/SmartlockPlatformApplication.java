c574a3a5d5cdfc267db661f9cdae549447d03f1a
ImaLi06 <imanollimache@gmail.com>
2026-06-14 20:22:10 -0500

    chore: initial commit

---
+++ src/main/java/com/smartindustries/smartlock/platform/SmartlockPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.smartindustries.smartlock.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlockPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlockPlatformApplication.class, args);
+    }
+
+}
