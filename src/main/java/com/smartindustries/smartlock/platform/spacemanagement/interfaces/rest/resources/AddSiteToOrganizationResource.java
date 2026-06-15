f8dce7ccb3bb7cf298c2cb0b2af1bf9764e2e99d
ImaLi06 <imanollimache@gmail.com>
2026-06-15 05:00:33 -0500

    feat(spacemanagement): create AddSiteToOrganizationResource

---
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/interfaces/rest/resources/AddSiteToOrganizationResource.java
@@ -0,0 +1,4 @@
+package com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.resources;
+
+public record AddSiteToOrganizationResource(Long organizationId, String name, String description) {
+}
