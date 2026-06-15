0e6e8faf06d8dcb554d2508c5fd30245a9a8c710
ImaLi06 <imanollimache@gmail.com>
2026-06-15 00:27:39 -0500

    feat(iam): add SignIn to UserCommandService

--- src/main/java/com/smartindustries/smartlock/platform/iam/application/commandservices/UserCommandService.java
+++ src/main/java/com/smartindustries/smartlock/platform/iam/application/commandservices/UserCommandService.java
@@ -1,10 +1,14 @@
 package com.smartindustries.smartlock.platform.iam.application.commandservices;
 
 import com.smartindustries.smartlock.platform.iam.domain.model.aggregates.User;
+import com.smartindustries.smartlock.platform.iam.domain.model.commands.SignInCommand;
 import com.smartindustries.smartlock.platform.iam.domain.model.commands.SignUpCommand;
 import com.smartindustries.smartlock.platform.shared.application.result.ApplicationError;
 import com.smartindustries.smartlock.platform.shared.application.result.Result;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public interface UserCommandService {
+    Result<ImmutablePair<User, String>, ApplicationError> handle(SignInCommand command);
+
     Result<User, ApplicationError> handle(SignUpCommand command);
 }
--- src/main/java/com/smartindustries/smartlock/platform/iam/application/internal/commandservices/UserCommandServiceImpl.java
+++ src/main/java/com/smartindustries/smartlock/platform/iam/application/internal/commandservices/UserCommandServiceImpl.java
@@ -4,12 +4,14 @@ import com.smartindustries.smartlock.platform.iam.application.commandservices.Us
 import com.smartindustries.smartlock.platform.iam.application.internal.outboundservices.hashing.HashingService;
 import com.smartindustries.smartlock.platform.iam.application.internal.outboundservices.tokens.TokenService;
 import com.smartindustries.smartlock.platform.iam.domain.model.aggregates.User;
+import com.smartindustries.smartlock.platform.iam.domain.model.commands.SignInCommand;
 import com.smartindustries.smartlock.platform.iam.domain.model.commands.SignUpCommand;
 import com.smartindustries.smartlock.platform.iam.domain.repositories.UserRepository;
 import com.smartindustries.smartlock.platform.shared.application.result.ApplicationError;
 import com.smartindustries.smartlock.platform.shared.application.result.Result;
 import com.smartindustries.smartlock.platform.shared.domain.model.valueobjects.Email;
 import com.smartindustries.smartlock.platform.shared.domain.model.valueobjects.Password;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,6 +25,27 @@ public class UserCommandServiceImpl implements UserCommandService {
         this.userRepository = userRepository;
         this.hashingService = hashingService;
         this.tokenService = tokenService;
+    }
+
+    @Override
+    public Result<ImmutablePair<User, String>, ApplicationError> handle(SignInCommand command) {
+        try {
+            var email = new Email(command.email());
+            var user = userRepository.findUserByEmail(email);
+            if (user.isEmpty()) {
+                return Result.failure(ApplicationError.notFound("User", command.email()));
+            }
+            if (!hashingService.matches(command.password(), user.get().getPassword())) {
+                return Result.failure(ApplicationError.validationError("credentials", "Invalid email or password"));
+            }
+
+            var token = tokenService.generateToken(user.get().getEmail().address());
+            return Result.success(ImmutablePair.of(user.get(), token));
+        } catch (IllegalArgumentException e) {
+            return Result.failure(ApplicationError.validationError("User", e.getMessage()));
+        } catch (Exception e) {
+            return Result.failure(ApplicationError.unexpected("sign-in", e.getMessage()));
+        }
     }
 
     @Override
