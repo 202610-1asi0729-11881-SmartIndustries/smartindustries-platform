7fc7f25e1ee266544e34d678a1d01745297e858d
ImaLi06 <imanollimache@gmail.com>
2026-06-16 02:15:36 -0500

    feat(spacemanagement): create resources for ConnectDeviceToOrganization

---
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/interfaces/rest/resources/DeviceResource.java
@@ -0,0 +1,4 @@
+package com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.resources;
+
+public record DeviceResource(Long id, Long siteId, String name, String status, String mode) {
+}
