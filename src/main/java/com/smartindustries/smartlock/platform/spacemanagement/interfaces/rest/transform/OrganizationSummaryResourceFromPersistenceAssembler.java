da78eea7b613808fcaee7364c5f4589fdfafc43d
ImaLi06 <imanollimache@gmail.com>
2026-06-16 11:14:55 -0500

    feat(spacemanagement):create OrganizationSummery assembler

---
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/interfaces/rest/transform/OrganizationSummaryResourceFromPersistenceAssembler.java
@@ -0,0 +1,25 @@
+package com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.transform;
+
+import com.smartindustries.smartlock.platform.spacemanagement.infrastructure.persistence.jpa.entities.OrganizationPersistenceEntity;
+import com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.resources.OrganizationSummaryResource;
+
+public final class OrganizationSummaryResourceFromPersistenceAssembler {
+
+    private OrganizationSummaryResourceFromPersistenceAssembler() {
+    }
+
+    public static OrganizationSummaryResource toResourceFromPersistence(OrganizationPersistenceEntity entity) {
+        var ownerName = entity.getRoles().stream()
+                .filter(r -> "Root Admin".equals(r.getName().value()))
+                .findFirst()
+                .flatMap(r -> r.getMemberships().stream().findFirst())
+                .map(m -> m.getUser().getName().getFullName())
+                .orElse("Unknown");
+
+        return new OrganizationSummaryResource(
+                entity.getId(),
+                entity.getName().value(),
+                entity.getDescription(),
+                ownerName);
+    }
+}
