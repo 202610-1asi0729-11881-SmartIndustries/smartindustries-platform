1e58f219f9e542464191ff7927a67fadbc9c6858
ImaLi06 <imanollimache@gmail.com>
2026-06-16 02:10:17 -0500

    feat(spacemanagement): create DeviceRepository

---
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/domain/repositories/DeviceRepository.java
@@ -0,0 +1,10 @@
+package com.smartindustries.smartlock.platform.spacemanagement.domain.repositories;
+
+import com.smartindustries.smartlock.platform.spacemanagement.domain.model.aggregates.Device;
+
+import java.util.Optional;
+
+public interface DeviceRepository {
+    Optional<Device> findById(Long id);
+    Device save(Device device);
+}
