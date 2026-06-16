5d2d06cbc57a171e1ce4796041a41d760e140938
ImaLi06 <imanollimache@gmail.com>
2026-06-16 13:23:55 -0500

    feat(spacemanagement): add GetSitesByOrganizationId to DeviceController

--- src/main/java/com/smartindustries/smartlock/platform/spacemanagement/interfaces/rest/DeviceController.java
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/interfaces/rest/DeviceController.java
@@ -2,10 +2,14 @@ package com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest;
 
 import com.smartindustries.smartlock.platform.shared.interfaces.rest.transform.ResponseEntityAssembler;
 import com.smartindustries.smartlock.platform.spacemanagement.application.commandservices.DeviceCommandService;
+import com.smartindustries.smartlock.platform.spacemanagement.application.queryservices.DeviceQueryService;
+import com.smartindustries.smartlock.platform.spacemanagement.domain.model.queries.GetDevicesByOrganizationIdQuery;
 import com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.resources.ConnectDeviceToSiteResource;
 import com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.resources.DeviceResource;
+import com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.resources.DeviceWithSiteResource;
 import com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.transform.ConnectDeviceToSiteCommandFromResourceAssembler;
 import com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.transform.DeviceResourceFromEntityAssembler;
+import com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.transform.DeviceWithSiteResourceFromPersistenceAssembler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,10 +19,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/devices", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -26,9 +29,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeviceController {
 
     private final DeviceCommandService deviceCommandService;
+    private final DeviceQueryService deviceQueryService;
 
-    public DeviceController(DeviceCommandService deviceCommandService) {
+    public DeviceController(DeviceCommandService deviceCommandService, DeviceQueryService deviceQueryService) {
         this.deviceCommandService = deviceCommandService;
+        this.deviceQueryService = deviceQueryService;
+    }
+
+    @GetMapping
+    @Operation(
+        summary = "Get devices by organization",
+        description = "Returns all devices belonging to a specific organization, including their site information."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Devices retrieved successfully",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = DeviceWithSiteResource.class)
+            )
+        )
+    })
+    public ResponseEntity<List<DeviceWithSiteResource>> getDevicesByOrganization(@RequestParam Long organizationId) {
+        var entities = deviceQueryService.handle(new GetDevicesByOrganizationIdQuery(organizationId));
+        var resources = entities.stream()
+                .map(DeviceWithSiteResourceFromPersistenceAssembler::toResourceFromPersistence)
+                .toList();
+        return ResponseEntity.ok(resources);
     }
 
     @PostMapping
