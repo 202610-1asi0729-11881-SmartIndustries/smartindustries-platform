b8313716bc16013ea82f39efece749dd1343c4e7
ImaLi06 <imanollimache@gmail.com>
2026-06-16 13:21:52 -0500

    feat(spacemanagement): add findBySite_Orgnaization_Id to DevicePersistenceRepository

--- src/main/java/com/smartindustries/smartlock/platform/spacemanagement/infrastructure/persistence/jpa/repositories/DevicePersistenceRepository.java
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/infrastructure/persistence/jpa/repositories/DevicePersistenceRepository.java
@@ -4,6 +4,9 @@ import com.smartindustries.smartlock.platform.spacemanagement.infrastructure.per
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DevicePersistenceRepository extends JpaRepository<DevicePersistenceEntity, Long> {
+    List<DevicePersistenceEntity> findBySite_Organization_Id(Long organizationId);
 }
