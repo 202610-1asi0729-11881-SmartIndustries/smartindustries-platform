3e904effa0a4ed2f535a38f1c9c862a7f0309af7
ImaLi06 <imanollimache@gmail.com>
2026-06-16 11:13:52 -0500

    feat(spacemanagement): create OrganizationSummeryResource

---
+++ src/main/java/com/smartindustries/smartlock/platform/spacemanagement/interfaces/rest/resources/OrganizationSummaryResource.java
@@ -0,0 +1,4 @@
+package com.smartindustries.smartlock.platform.spacemanagement.interfaces.rest.resources;
+
+public record OrganizationSummaryResource(Long id, String name, String description, String ownerName) {
+}
