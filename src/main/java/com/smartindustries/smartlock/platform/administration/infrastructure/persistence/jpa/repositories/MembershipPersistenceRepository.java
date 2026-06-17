2dab4e0208fdde8f886d1f925efe30a4a5c36481
ImaLi06 <imanollimache@gmail.com>
2026-06-17 04:18:54 -0500

    feat(administration) add findByRole_Organization_Id to MemebershipPersistenceRepository

--- src/main/java/com/smartindustries/smartlock/platform/administration/infrastructure/persistence/jpa/repositories/MembershipPersistenceRepository.java
+++ src/main/java/com/smartindustries/smartlock/platform/administration/infrastructure/persistence/jpa/repositories/MembershipPersistenceRepository.java
@@ -4,6 +4,9 @@ import com.smartindustries.smartlock.platform.administration.infrastructure.pers
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MembershipPersistenceRepository extends JpaRepository<MembershipPersistenceEntity, Long> {
+    List<MembershipPersistenceEntity> findByRole_Organization_Id(Long organizationId);
 }
