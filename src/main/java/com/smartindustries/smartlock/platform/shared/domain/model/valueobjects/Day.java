55cd82e6393a8d2f6f762d4d81994a8a75602383
ImaLi06 <imanollimache@gmail.com>
2026-06-17 12:37:08 -0500

    feat(shared): create Day value object

---
+++ src/main/java/com/smartindustries/smartlock/platform/shared/domain/model/valueobjects/Day.java
@@ -0,0 +1,11 @@
+package com.smartindustries.smartlock.platform.shared.domain.model.valueobjects;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
